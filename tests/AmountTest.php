b7331051164f52a3637eedefdd6e96258ff293e9
Ian Rodrigues <iansrodrigs@gmail.com>
2018-03-26 08:29:07 -0300

    test Amount

---
+++ tests/AmountTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use IanRodrigues\DIBSPayment\Amount;
+use PHPUnit\Framework\TestCase;
+
+class AmountTest extends TestCase
+{
+    public function testGetters()
+    {
+        $amount = new Amount('usd', 10000);
+
+        $this->assertEquals('USD', $amount->getCurrency());
+        $this->assertEquals(10000, $amount->getValue());
+    }
+}
