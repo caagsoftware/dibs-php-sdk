baf2a5bc654cb2d19567df7df62e762706e76ea4
Ian Rodrigues <iansrodrigs@gmail.com>
2018-03-26 09:08:18 -0300

    test Transact result

---
+++ tests/Flexwin/ResultTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use IanRodrigues\DIBSPayment\Flexwin\Result;
+use PHPUnit\Framework\TestCase;
+
+class ResultTest extends TestCase
+{
+    public function testGetters()
+    {
+        $result = new Result([
+            'statuscode' => 5,
+            'approvalcode' => '112233',
+            'transact' => '123456',
+        ]);
+
+        $this->assertTrue($result->isApproved());
+        $this->assertEquals(112233, $result->getApprovalCode());
+        $this->assertEquals(123456, $result->getTransactId());
+        $this->assertEquals('The transaction capture is approved by the acquirer.', $result->getMessage());
+    }
+}
