87b603823b4c91147d4612614207f4617fa5c0b0
Ian Rodrigues <iansrodrigs@gmail.com>
2018-03-26 09:35:39 -0300

    test Refund request

---
+++ tests/Requests/PaymentHandling/RefundTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use IanRodrigues\DIBSPayment\Amount;
+use IanRodrigues\DIBSPayment\Requests\PaymentHandling\Refund;
+use PHPUnit\Framework\TestCase;
+
+class RefundTest extends TestCase
+{
+    public function testGetters()
+    {
+        $refund = new Refund(123456, 'order-1234', new Amount('usd', 10000));
+
+        $this->assertEquals('POST', $refund->getHttpMethod());
+        $this->assertEquals('/cgi-adm/refund.cgi', $refund->getEndpoint());
+        $this->assertEquals([
+            'transact' => 123456,
+            'orderid' => 'order-1234',
+            'currency' => 'USD',
+            'amount' => 10000,
+            'textreply' => 'success',
+        ], $refund->toBodyParams());
+    }
+}
