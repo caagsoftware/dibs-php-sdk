7368c18b2adbb1f60ec11e9175b8a451bede8183
Ian Rodrigues <iansrodrigs@gmail.com>
2018-03-26 07:29:21 -0300

    tests for DIBS

---
+++ tests/DIBSTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use IanRodrigues\DIBSPayment\Amount;
+use IanRodrigues\DIBSPayment\DIBS;
+use IanRodrigues\DIBSPayment\Requests\PaymentHandling\Refund;
+use PHPUnit\Framework\TestCase;
+
+class DIBSTest extends TestCase
+{
+    public function testGetters()
+    {
+        $dibs = new DIBS('merchant-id', 'merchant-secret');
+
+        $this->assertEquals('merchant-id', $dibs->getMerchantId());
+        $this->assertEquals('merchant-secret', $dibs->getMerchantSecret());
+    }
+
+    public function testTestEnvironment()
+    {
+        $dibs = new DIBS('merchant-id', 'merchant-secret');
+
+        $this->assertFalse($dibs->isTestEnvironment());
+
+        $dibs->setTestEnvironment();
+        $this->assertTrue($dibs->isTestEnvironment());
+    }
+
+    public function testGetBodyParams()
+    {
+        $dibs = new DIBS('merchant-id', 'merchant-secret');
+
+        $this->assertEquals([
+            'merchant' => 'merchant-id',
+        ], $dibs->toBodyParams());
+
+        $dibs->setTestEnvironment();
+        $this->assertEquals([
+            'merchant' => 'merchant-id',
+            'test' => 1,
+        ], $dibs->toBodyParams());
+    }
+
+    public function testHandleDIBSRequests()
+    {
+        $dibs = new DIBS('merchant-id', 'merchant-secret');
+        $refund = new Refund('123456', 'order-1234', new Amount('usd', 2990));
+
+        $result = $dibs->handle($refund, new class {
+            public function handle($request)
+            {
+                return ['status_code' => 0];
+            }
+        });
+
+        $this->assertEquals(0, $result['status_code']);
+    }
+}
