5965af93531d95442d434455d2e46a0a40a0bbf2
nadin <nadin@caagcrm.com>
2018-07-23 22:43:20 -0400

    missing commit

--- src/Requests/Handler.php
+++ src/Requests/Handler.php
@@ -7,6 +7,24 @@ use CaagSoftware\DIBSPayment\DIBS;
 
 class Handler
 {
+    public static $response_codes = [
+        '0' => 'Refund Completed',
+        '1' => 'No response from acquirer',
+        '2' => 'Timeout',
+        '3' => 'Credit card expired',
+        '4' => 'Rejected by acquirer',
+        '5' => 'Authorisation older than 7 days',
+        '6' => 'Transaction status on the DIBS server does not allow function',
+        '7' => 'Amount too high',
+        '8' => 'Error in the parameters sent to the DIBS server. An additional parameter called "message" is returned, with a value that may help identifying the error',
+        '9' => 'Order number (orderid) does not correspond to the authorisation order number',
+        '10' => 'Re-authorisation of the transaction was rejected',
+        '11' => 'Not able to communicate with the acquier',
+        '12' => 'Confirm request error',
+        '14' => 'Capture is called for a transaction which is pending for batch - i.e. capture was already called.',
+        '15' => 'Capture or refund was blocked by DIBS',
+    ];
+
     /**
      * @var DIBS
      */
@@ -31,7 +49,7 @@ class Handler
     }
 
     /**
-     * Handle the request.
+     * Handle the request
      *
      * @param Request $request
      *
@@ -49,6 +67,6 @@ class Handler
 
         parse_str($response->getBody()->getContents(), $result);
 
-        return $result;
+        return $result + ['response_message' => array_get(static::$response_codes, array_get($result, 'reason'))];
     }
 }
